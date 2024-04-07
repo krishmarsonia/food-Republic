bc10f9715aebc2a81f33e93e6acf2e6489d145ec
krishmarsonia <krishmarsonia@gmail.com>
2024-04-08 00:37:25 +0530

    uri changed for graphql server

--- src/providers/graphqlProvider.tsx
+++ src/providers/graphqlProvider.tsx
@@ -5,7 +5,7 @@ import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 export const Provider = ({ children }: { children: any }) => {
   const port  = process.env.PORT || 3000
   const client = new ApolloClient({
-    uri: `https://localhost:${port}/api/graphql`,
+    uri: `https://food-republic.vercel.app/api/graphql`,
     cache: new InMemoryCache(),
   });
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
